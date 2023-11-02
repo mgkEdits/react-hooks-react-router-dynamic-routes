c044762a2cbad67c6323d33c83c1203673c5e367
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-11-02 21:53:52 +0300

    code along complete

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -1,22 +1,27 @@
 import React from "react";
 import { Route, useRouteMatch } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import MoviesList from "./MoviesList";
 import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
   const match = useRouteMatch();
+  console.log(match);
+
+  const params = useParams();
+  console.log(params);
+
 
   return (
     <div>
-      <MoviesList movies={movies} />
-
-      <Route exact path={match.url}>
-        <h3>Choose a movie from the list above</h3>
-      </Route>
 
       <Route path={`${match.url}/:movieId`}>
-        <MovieShow movies={movies} />
+        <MovieShow />
       </Route>
+
+      <div>
+      <h3>{movies[params.movieId].title}</h3>
+    </div>
     </div>
   );
 }
